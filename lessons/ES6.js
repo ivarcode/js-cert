9ea15757e0f0b21deb465c323721d8846dd72ee7
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-06-17 23:19:53 -0400

    6/17

--- lessons/ES6.js
+++ lessons/ES6.js
@@ -1,2 +1,62 @@
 /* functions from ES6 section */
 
+let catName;
+let quote;
+function catTalk() {
+    "use strict";
+
+    catName = "Oliver";
+    quote = catName + " says Meow!";
+
+}
+catTalk();
+
+
+
+function checkScope() {
+    "use strict";
+    let i = "function scope";
+    if (true) {
+        let i = "block scope";
+        console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+}
+
+
+
+function printManyTimes(str) {
+    "use strict";
+
+    // change code below this line
+
+    const SENTENCE = str + " is cool!";
+    for (let i = 0; i < str.length; i += 2) {
+        console.log(SENTENCE);
+    }
+
+    // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
+
+
+
+const s = [5, 7, 2];
+function editInPlace() {
+    "use strict";
+    // change code below this line
+
+    s[0] = s[2];
+    s[1] = 5;
+    s[2] = 7;
+
+    // change code above this line
+}
+editInPlace();
+
+
+
+
+
