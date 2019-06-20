d7b23d457652c8b11e0684576d35b1e079e3be4b
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-06-19 22:22:21 -0400

    6/19

--- lessons/RegEx.js
+++ lessons/RegEx.js
@@ -91,3 +91,8 @@ let lastRegex = /caboose$/; // Change this line
 let result = lastRegex.test(caboose);
 
 
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/gi; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
+
+
