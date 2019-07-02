9e7fe1768bc144a7188d744273fbc72501783306
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-07-01 23:03:14 -0400

    fixed palindrome using regex woo

--- final_project/palindrome_checker.js
+++ final_project/palindrome_checker.js
@@ -1,23 +1,8 @@
-// returns true if param str is a palindrome and false if str is not
 function palindrome(str) {
-    let parsedStr = "";
-    for (let i = 0; i < str.length; i++) {
-        console.log(str.charAt(i));
-        console.log(str.charCodeAt(i));
-        //is upper
-        if (str.charCodeAt(i) < 91 && str.charCodeAt(i) > 64) {
-            //make lower
-            parsedStr += String.fromCharCode(str.charCodeAt(i) + 32);
-        }
-        //is lower || number
-        if ((str.charCodeAt(i) < 123 && str.charCodeAt(i) > 96) || (str.charCodeAt(i) < 58 && str.charCodeAt(i) > 47)) {
-            parsedStr += str.charAt(i);
-        }
-    }
+    let parsedStr = str.toLowerCase().trim().match(/(\d|[a-z])+/g).join('');
     console.log(parsedStr);
     let a = 0;
     let b = parsedStr.length - 1;
-    console.log(a + " " + b);
     // while b and a have not overlapped
     while (b > a) {
         if (parsedStr.charCodeAt(a) == parsedStr.charCodeAt(b)) {
@@ -31,4 +16,5 @@ function palindrome(str) {
 }
 
 
-console.log(palindrome("raceca"));
+
+palindrome("not a palindrome");
